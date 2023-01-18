50469d755921ccba195aa8e2bd33f2c50e6578ad
yesk31 <yesk31@naver.com>
2023-01-19 03:45:39 +0900

    test : RTC CODE

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const { createClient } = require("redis");
 const { log } = require("console");
-const e = require("express");
 
 dotenv.config();
 app.use(cors());
@@ -30,7 +29,7 @@ connectClient()
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     method: ["GET", "POST"],
   },
 });
@@ -39,7 +38,7 @@ let userCount = 0;
 let readyCount = 0;
 
 io.on("connection", async (socket) => {
-  console.log("connect",socket.id);
+  console.log("connect", socket.id);
   socket.onAny(async (e) => {
     console.log(`SocketEvent:${e}`);
   });
@@ -69,7 +68,8 @@ io.on("connection", async (socket) => {
     console.log(roomInfo);
   });
 
-  socket.on("ready-to-join", async ({ userId, roomId }) => {
+  socket.on("ready", async ({ userId, roomId }) => {
+    // FIXME: 변수 -> redis.lenth로 변경 필요
     const userInfo = await client.hGetAll(
       `rooms:${roomId}:users:${socket.userId}`
     );
@@ -81,10 +81,18 @@ io.on("connection", async (socket) => {
       readyCount = readyCount + 1;
       await client.hSet(`rooms:${roomId}`, { ready: readyCount });
       const test = await client.hGetAll(`rooms:${roomId}`);
-      console.log("+했을때 값 확인", test);
+
       if (readyCount > 3) {
+        // room 초기화
+        // 명령어를 못찾는 이유
+        for (let i = 0; i < 13; i++) {
+          await client.hmset(`rooms:${roomId}:table:black`, {
+            color: black,
+            value: i,
+            isOpened: false,
+          });
+        }
         io.to(roomId).emit("game-start");
-        console.log("게임 시작");
       }
     } else {
       await client.hSet(`rooms:${roomId}:users:${socket.userId}`, {
@@ -93,18 +101,86 @@ io.on("connection", async (socket) => {
 
       readyCount = readyCount - 1;
       await client.hSet(`rooms:${roomId}`, { ready: readyCount });
-
       const test = await client.hGetAll(`rooms:${roomId}`);
-      console.log("-했을때 값 확인", test);
     }
   });
 
-  socket.on("first-draw", ({ userId, black, roomId }) => {});
+  socket.on("first-draw", ({ userId, black, roomId }) => {
+    const whiteCard = 3 - black;
 
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
+    let count = 0;
+    let arr1 = [];
+    let flag = 0;
+
+    for (let j = 0; j < data.length; j++) {
+      if (data[j].roomId === roomId) {
+        flag = j;
+        break;
+      }
+    }
+
+    for (let i = 0; count < black; i++) {
+      const number = Math.floor(Math.random() * 12);
+
+      if (data[flag].blackCardList[number] === null) {
+        data[flag].blackCardList[number] = userId;
+        arr1 = [...arr1, { color: "black", value: number }];
+        count++;
+      }
+    }
+
+    count = 0;
+    for (let i = 0; count < whiteCard; i++) {
+      number = Math.floor(Math.random() * 12);
+
+      if (data[flag].whiteCardList[number] === null) {
+        data[flag].whiteCardList[number] = userId;
+        arr1 = [...arr1, { color: "white", value: number }];
+        count++;
+      }
+    }
+
+    countBlack = 0;
+    countWhite = 0;
+    for (let i = 0; i < data[flag].blackCardList.length; i++) {
+      if (data[flag].blackCardList[i] !== null) {
+        countBlack++;
+      }
+
+      if (data[flag].whiteCardList[i] !== null) {
+        countWhite++;
+      }
+    }
+    socket["card"] = arr1;
+
+    socket.card
+      .sort((a, b) => a.value - b.value)
+      .sort((a, b) => {
+        if (a.value === b.value) {
+          if (a.color < b.color) return -1;
+          else if (b.color < a.color) return 1;
+          else return 0;
+        }
+      });
+
+    const userIdAndCard = { userId, cards: socket.card };
+
+    for (let i = 0; i < data.length; i++) {
+      //오류난 부분 체크필요 ㄱ
+      if (data[flag].roomData[i].userId == userId) {
+        data[flag].roomData[i].cards = socket.card;
+        break;
+      }
+    }
+
+    if (data[flag].roomData.length === 4) {
+      socket
+        .to(roomId)
+        .emit("allUsersFirstCard", data, { countBlack, countWhite });
+    }
+  });
+
+ 
 
   socket.on("send_message", (data, addMyMessage) => {
     socket.to(data.room).emit("receive_message", data.msg);
@@ -150,10 +226,7 @@ io.on("connection", async (socket) => {
     }
   });
 
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
-  // TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:
+  
 
   socket.on("gameStart", (roomId, userId) => {
     socket["userId"] = socket.id;
