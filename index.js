9348b8c3337381dcd46ef65cf8b7ff4d6d5279b5
yesk31 <yesk31@naver.com>
2023-01-20 05:11:19 +0900

    test: EC2 test

--- index.js
+++ index.js
@@ -38,20 +38,64 @@ const io = new Server(server, {
 let userCount = 0;
 let readyCount = 0;
 
-//DB room 에 userId, table에 blackCards[] , whiteCards[]
-//DB user 에 userId(key)으로 socketId, userName, isReady, isAlive, hand: Card[];
- 
+// let DB = [
+// {
+//   rooms: [
+//     {
+//       roomId: 0,
+//       blackCards: new Array(13).fill(null),
+//       whiteCards: new Array(13).fill(null), // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+//       users: [], //[{userId:1}, {userId:4}, {userId:3}, {userId:2}]
+//     },
+//   ],
+//   users: [
+//     {
+//       userId,
+//       sids,
+//       username,
+//       isReady,
+//       isAlive,
+//       hand: [],
+//     },
+//   ],
+//   }
+// ];
+
+let DB = [
+  {
+    roomId: 0,
+
+    table: {
+        blackCards:[0,1,2,3,4,5,6,7,8,9,10,11,12],
+        whiteCards:[0,1,2,3,4,5,6,7,8,9,10,11,12], 
+        users: [], //[{userId:1}, {userId:4}, {userId:3}, {userId:2}]
+      
+    },
+
+    users: [
+      {
+        userId: 0,
+        sids:0,
+        username:0,
+        isReady:false,
+        isAlive: true,
+        hand: [], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+      },
+    ],
+  },
+];
+
+let countBlack = 0;
+let countWhite = 0;
 
 io.on("connection", async (socket) => {
   console.log("connect", socket.id);
   socket.onAny(async (e) => {
     console.log(`SocketEvent:${e}`);
   });
-    socket.on("connect", ({ userId }) =>{
-        //DB room 돌면서 userId 있는지 확인하고 삭제
-        
-    })
-
+  socket.on("connect", (userId) => {
+    //DB room 돌면서 userId 있는지 확인하고 삭제
+  });
 
   socket.on("you-joined", async ({ userID, roomID }) => {
     socket["userID"] = userID;
@@ -103,8 +147,8 @@ io.on("connection", async (socket) => {
       let userLength = await client.hLen(`rooms:${roomID}`);
       userLength = 2;
 
-      if (userLength ==  2) {
-        console.log("test")
+      if (userLength == 2) {
+        console.log("test");
         // 유저 별로 socket.Id 찾아서 뿌려주기.
         io.to(socket.id).emit("game-start");
       }
@@ -114,140 +158,106 @@ io.on("connection", async (socket) => {
       const test2 = await client.hGetAll(`rooms:${roomID}`);
       console.log("삭제", test2);
     }
-
-    //console.log(userInfo);
-
-    //   readyCount = readyCount + 1;
-
-    //   await client.hSet(`rooms:${roomId}`, { userId });
-    //   const ready = await client.hGetAll(`rooms:${roomId}`);
-    //   console.log("+일 때 ready 값: ", ready);
-
-    // if (readyCount > 3) {
-    //   // room 초기화
-    //   for (let i = 0; i < 13; i++) {
-    //     await client.hSet(`rooms:${roomId}:table:black`, {
-    //       color: "black",
-    //       value: `${i}`,
-    //       isOpened: "false",
-    //     });
-
-    //     await client.hSet(`rooms:${roomId}:table:white`, {
-    //       color: "white",
-    //       value: `${i}`,
-    //       isOpened: "false",
-    //     });
-    //   }
-
-    //   io.to(roomId).emit("game-start");
-    // }
-    //}
-
-    // else {
-    //   await client.hSet(`rooms:${roomId}:users:${socket.userId}`, {
-    //     isReady: "false",
-    //   });
-
-    //   readyCount = readyCount - 1;
-    //   await client.hDel(`rooms:${roomId}`, { userId });
-    //   const ready = await client.hGetAll(`rooms:${roomId}`);
-    //   console.log("-일 때 ready 값: ", ready);
-    // }
   });
 
-  socket.on("first-draw", async ({ userId, black, roomId }) => {
-    const whiteCard = 3 - black;
-
-    console.log("userId", userId)
-    console.log("black", black)
-    console.log("roomId", roomId)
- 
-
-    console.log("socket ids:", socket.id);
-    const tableWhiteCard = await client.hGetAll(`rooms:${roomId}:table:white`);
-    console.log("tableWhiteCard test console :::", tableWhiteCard);
+  socket.on("first-draw", async (userId, black, roomId, fn) => {
+    console.log(DB[0].table.blackCards)
+    // fn (본인 카드 & 잔여 카드 )  
+    // socket.to(roomId).emit("all-users-cards", [사람들 카드 + 잔여 카드])
+    const white = 3 - black;
 
     let count = 0;
-    let arr1 = [];
+    let getCards = [];
     let flag = 0;
 
-    for (let j = 0; j < data.length; j++) {
-      if (data[j].roomId === roomId) {
+    for (let j = 0; j < DB.length; j++) {
+      if (DB[j].roomId === roomId) {
         flag = j;
         break;
       }
     }
 
-    for (let i = 0; count < black; i++) {
-      const number = Math.floor(Math.random() * 12);
-
-      if (data[flag].blackCardList[number] === null) {
-        data[flag].blackCardList[number] = userId;
-        arr1 = [...arr1, { color: "black", value: number }];
-        count++;
-      }
+    for(let i=0; count < black; i++){
+      const cardLength = DB[j].table.blackCards.length
+      const randomCard =  Math.floor(Math.random() * cardLength);
+      getCards = [...getCards, { color: "black", value: randomCard }];
+      
     }
 
-    count = 0;
-    for (let i = 0; count < whiteCard; i++) {
-      number = Math.floor(Math.random() * 12);
 
-      if (data[flag].whiteCardList[number] === null) {
-        data[flag].whiteCardList[number] = userId;
-        arr1 = [...arr1, { color: "white", value: number }];
-        count++;
-      }
-    }
+    // for (let i = 0; count < black; i++) {
+    //   const number = Math.floor(Math.random() * 12);
 
-    countBlack = 0;
-    countWhite = 0;
-    for (let i = 0; i < data[flag].blackCardList.length; i++) {
-      if (data[flag].blackCardList[i] !== null) {
-        countBlack++;
-      }
+    //   if (data[flag].blackCardList[number] === null) {
+    //     data[flag].blackCardList[number] = userId;
+    //     arr1 = [...arr1, { color: "black", value: number }];
+    //     count++;
+    //   }
+    // }
 
-      if (data[flag].whiteCardList[i] !== null) {
-        countWhite++;
-      }
-    }
-    socket["card"] = arr1;
+   
 
-    socket.card
-      .sort((a, b) => a.value - b.value)
-      .sort((a, b) => {
-        if (a.value === b.value) {
-          if (a.color < b.color) return -1;
-          else if (b.color < a.color) return 1;
-          else return 0;
-        }
-      });
+    // count = 0;
+    // for (let i = 0; count < whiteCard; i++) {
+    //   number = Math.floor(Math.random() * 12);
 
-    const userIdAndCard = { userId, cards: socket.card };
+    //   if (data[flag].whiteCardList[number] === null) {
+    //     data[flag].whiteCardList[number] = userId;
+    //     arr1 = [...arr1, { color: "white", value: number }];
+    //     count++;
+    //   }
+    // }
 
-    for (let i = 0; i < data.length; i++) {
-      //오류난 부분 체크필요 ㄱ
-      if (data[flag].roomData[i].userId == userId) {
-        data[flag].roomData[i].cards = socket.card;
-        break;
-      }
-    }
+    // countBlack = 0;
+    // countWhite = 0;
+    // for (let i = 0; i < data[flag].blackCardList.length; i++) {
+    //   if (data[flag].blackCardList[i] !== null) {
+    //     countBlack++;
+    //   }
 
-    if (data[flag].roomData.length === 4) {
-      socket
-        .to(roomId)
-        .emit("allUsersFirstCard", data, { countBlack, countWhite });
-    }
+    //   if (data[flag].whiteCardList[i] !== null) {
+    //     countWhite++;
+    //   }
+    // }
+    // socket["card"] = arr1;
+
+    // socket.card
+    //   .sort((a, b) => a.value - b.value)
+    //   .sort((a, b) => {
+    //     if (a.value === b.value) {
+    //       if (a.color < b.color) return -1;
+    //       else if (b.color < a.color) return 1;
+    //       else return 0;
+    //     }
+    //   });
+
+    // const userIdAndCard = { userId, cards: socket.card };
+
+    // for (let i = 0; i < data.length; i++) {
+    //   //오류난 부분 체크필요 ㄱ
+    //   if (data[flag].roomData[i].userId == userId) {
+    //     data[flag].roomData[i].cards = socket.card;
+    //     break;
+    //   }
+    // }
+
+    // if (data[flag].roomData.length === 4) {
+    //   socket
+    //     .to(roomId)
+    //     .emit("allUsersFirstCard", data, { countBlack, countWhite });
+    // }
   });
 
-  socket.on("send_message", (msg, room, addMyMessage) => {
+  socket.on("send-message", (msg, room, addMyMessage) => {
     console.log(msg);
     console.log(room);
+    // 소켓 아이디에 맞는 닉네임을 뽑아서 msg와 같이 전송
 
-    socket.to(room).emit("receive_message", msg);
+    socket.to(room).emit("receive-message", msg);
     addMyMessage(msg);
   });
 
-  socket.on("joined", (roomId) => {
+  socket.on("join", (roomId) => {
     console.log(roomId);
     socket.join(roomId);
     // if (users[roomID]) {
@@ -292,16 +302,24 @@ io.on("connection", async (socket) => {
     const whiteCard = 3 - black;
 
     let count = 0;
-    let arr1 = [];
+    let getCards = [];
     let flag = 0;
 
-    for (let j = 0; j < data.length; j++) {
-      if (data[j].roomId === roomId) {
+    for (let j = 0; j < DB.length; j++) {
+      if (DB[j].roomId === roomId) {
         flag = j;
         break;
       }
     }
 
+    for(let i=0; count < black; i++){
+      const cardLength = DB[j].table.blackCards.length
+      const randomCard =  Math.floor(Math.random() * cardLength);
+      getCards = [...getCards, { color: "black", value: randomCard }];
+
+    }
+
+    
     for (let i = 0; count < black; i++) {
       const number = Math.floor(Math.random() * 12);
 
