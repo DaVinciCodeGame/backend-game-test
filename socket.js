ff46f908c89dc2239f09fd464490822e7d7a5978
origin <yesk31@naver.com>
2023-01-25 15:29:46 +0900

    메인 서버 올리기 위한 코드정리

--- socket.js
+++ socket.js
@@ -4,7 +4,6 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { createClient } = require("redis");
 const { log } = require("console");
 const { type } = require("os");
 const connect = require("./schemas");
@@ -12,22 +11,6 @@ connect();
 
 dotenv.config();
 app.use(cors());
-
-//redis 기본 정리 및 연결 시작
-const client = createClient({
-  url: process.env.REDIS_HOST,
-});
-const connectClient = async () => {
-  return await client.connect();
-};
-
-connectClient()
-  .then(async () => {
-    await client.set("resdi", "연결 test");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
 
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -69,43 +52,26 @@ io.on("connection", async (socket) => {
     console.log(`SocketEvent:${e}`);
   });
 
-  socket.on("send-message", (msg, room, addMyMessage) => {
+  socket.on("send-message", (msg, room, nickName, addMyMessage) => {
     console.log(msg);
     console.log(room);
     // 소켓 아이디에 맞는 닉네임을 뽑아서 msg와 같이 전송
 
-    socket.to(room).emit("receive-message", msg);
-    addMyMessage(msg);
+    socket.to(room).emit("receive-message", nickName, msg);
+    addMyMessage(nickName, msg);
   });
 
   socket.on("connect", (userId) => {
     //DB room 돌면서 userId 있는지 확인하고 삭제
   });
 
-  socket.on("you-joined", async ({ userID, roomID }) => {
-    
-    userCount = (userCount + 1) % 4;
-    console.log("유저 입장", userCount, ":::::", socket.id);
-
-    await client.hSet(`rooms:${roomID}:users:${socket.userID}`, {
-      userID: socket.userID,
-    });
-    await client.hSet(`rooms:${roomID}:users:${socket.userID}`, {
-      socketId: socket.id,
-    });
-    await client.hSet(`rooms:${roomID}:users:${socket.userID}`, {
-      username: userCount,
-    });
-    await client.hSet(`rooms:${roomID}:users:${socket.userID}`, {
-      isReady: "false",
-    });
-    await client.hSet(`rooms:${roomID}:users:${socket.userID}`, {
-      yourSeat: userCount,
-    });
-    const roomInfo = await client.hGetAll(
-      `rooms:${roomID}:users:${socket.userID}`
-    );
-    console.log(roomInfo);
+  socket.on("join", (roomId) => {
+    // TODO:
+    // game-info 필요
+    // roomId에 따른 방 제목 -> 게임 시작시 상단 바 정보(비공개, 인원, 방제목)
+    console.log(roomId);
+    socket.join(roomId);
+    socket.data.roomId = roomId;
   });
 
   socket.on("ready", async ({ userID, roomID }) => {
@@ -143,6 +109,8 @@ io.on("connection", async (socket) => {
       const test2 = await client.hGetAll(`rooms:${roomID}`);
       console.log("삭제", test2);
     }
+
+    // TODO: ADD_READY :: 방에 설정된 인원값이 모두 ready 했을 때 정보 보내기 + GAME_START 이벤트
   });
 
   socket.on("first-draw", async (userId, black, roomId, myCard) => {
@@ -161,6 +129,7 @@ io.on("connection", async (socket) => {
       }
     }
 
+    // black 뽑기
     for (let i = 0; i < black; i++) {
       let cardLength = DB[roomIndex].table.blackCards.length;
       let CardIndex = Math.floor(Math.random() * Number(cardLength));
@@ -172,6 +141,7 @@ io.on("connection", async (socket) => {
       DB[roomIndex].table.blackCards.splice(CardIndex, 1);
     }
 
+    // white 뽑기
     for (let i = 0; i < white; i++) {
       let cardLength = DB[roomIndex].table.whiteCards.length;
       let CardIndex = Math.floor(Math.random() * Number(cardLength));
@@ -203,193 +173,23 @@ io.on("connection", async (socket) => {
     myCard(getCards);
 
     // FIXME 나머지 사람들의 카드 보내주기
+    // forEach_myCard(data);
+    // io.to.(개인의 socket.Id).emit("draw-result", gameInfo)
     let sendAllData = {};
+  });
 
-  // TODO: 뽑을 카드가 없다.
-    if(DB.table.blackCards.length === 0 && DB.table.whiteCards.length === 0){
-      const turn = DB[flag].turn;
-
-      socket.to(socket.data.roomId).emit("no-cards-to-draw", "SELECT_CARD_AS_SECURITY", turn)
+  socket.on("color-selected", (userId, color) => {
+    if (color === "black") {
+    } else {
     }
 
-    
-    // TODO: 뽑을 카드가 있는가?
-    // TODO: 양쪽 색이 다 있는가 ? -> client 색 정하기 socket.to(roomId).emit("select-color", )
-    // TODO: 뽑힌 카드가 조커인가? 숫자인가?
-    // TODO: 각 요청마다 현재 게임을 진행중인 사람의 턴 첨부
-    
-    // const userIdAndCard = { userId, cards: socket.card };
-
-    // for (let i = 0; i < data.length; i++) {
-    //   //오류난 부분 체크필요 ㄱ
-    //   if (data[flag].roomData[i].userId == userId) {
-    //     data[flag].roomData[i].cards = socket.card;
-    //     break;
-    //   }
-    // }
-
-    // if (data[flag].roomData.length === 4) {
-    //   socket
-    //     .to(roomId)
-    //     .emit("allUsersFirstCard", data, { countBlack, countWhite });
-    // }
+    // io.to.(개인의 socket.Id).emit("selected-result", gameInfo)
   });
 
-  socket.on("select-card-as-security",(userId, color, value) => {
-    socket.data.security= {color:color, value:value};
-    io.to(socket.Id).emit("select-target")
-  })
-
-  socket.on("join", (roomId) => {
-    console.log(roomId);
-    socket.join(roomId);
-    socket.data.roomId = roomId
+  socket.on("select-card-as-security", (userId, color, value) => {
+    socket.data.security = { color: color, value: value };
+    io.to(socket.Id).emit("select-target");
   });
-
-  socket.on("sending signal", (payload) => {
-    io.to(payload.userToSignal).emit("user joined", {
-      signal: payload.signal,
-      callerID: payload.callerID,
-    });
-  });
-  socket.on("returning signal", (payload) => {
-    io.to(payload.callerID).emit("receiving returned signal", {
-      signal: payload.signal,
-      id: socket.id,
-    });
-  });
-
-  socket.on("disconnect-signal", () => {
-    const roomID = socketToRoom[socket.id];
-    let room = users[roomID];
-    if (room) {
-      room = room.filter((id) => id !== socket.id);
-      users[roomID] = room;
-    }
-  });
-
-  // socket.on("selectFirstCard", ({ userId, black, roomId }, addMyCard) => {
-  //   const whiteCard = 3 - black;
-
-  //   let count = 0;
-  //   let getCards = [];
-  //   let flag = 0;
-
-  //   for (let j = 0; j < DB.length; j++) {
-  //     if (DB[j].roomId === roomId) {
-  //       flag = j;
-  //       break;
-  //     }
-  //   }
-
-  //   for(let i=0; count < black; i++){
-  //     const cardLength = DB[j].table.blackCards.length
-  //     const randomCard =  Math.floor(Math.random() * cardLength);
-  //     getCards = [...getCards, { color: "black", value: randomCard }];
-
-  //   }
-
-  //   for (let i = 0; count < black; i++) {
-  //     const number = Math.floor(Math.random() * 12);
-
-  //     if (data[flag].blackCardList[number] === null) {
-  //       data[flag].blackCardList[number] = userId;
-  //       arr1 = [...arr1, { color: "black", value: number }];
-  //       count++;
-  //     }
-  //   }
-
-  //   count = 0;
-  //   for (let i = 0; count < whiteCard; i++) {
-  //     number = Math.floor(Math.random() * 12);
-
-  //     if (data[flag].whiteCardList[number] === null) {
-  //       data[flag].whiteCardList[number] = userId;
-  //       arr1 = [...arr1, { color: "white", value: number }];
-  //       count++;
-  //     }
-  //   }
-
-  //   countBlack = 0;
-  //   countWhite = 0;
-  //   for (let i = 0; i < data[flag].blackCardList.length; i++) {
-  //     if (data[flag].blackCardList[i] !== null) {
-  //       countBlack++;
-  //     }
-
-  //     if (data[flag].whiteCardList[i] !== null) {
-  //       countWhite++;
-  //     }
-  //   }
-  //   socket["card"] = arr1;
-
-  //   socket.card
-  //     .sort((a, b) => a.value - b.value)
-  //     .sort((a, b) => {
-  //       if (a.value === b.value) {
-  //         if (a.color < b.color) return -1;
-  //         else if (b.color < a.color) return 1;
-  //         else return 0;
-  //       }
-  //     });
-
-  //   const userIdAndCard = { userId, cards: socket.card };
-
-  //   for (let i = 0; i < data.length; i++) {
-  //     // FIXME: error
-  //     if (data[flag].roomData[i].userId == userId) {
-  //       data[flag].roomData[i].cards = socket.card;
-  //       break;
-  //     }
-  //   }
-
-  //   if (data[flag].roomData.length === 4) {
-  //     socket
-  //       .to(roomId)
-  //       .emit("allUsersFirstCard", data, { countBlack, countWhite });
-  //   }
-  // });
-
-  // socket.on("selectCard", (roomId, userId, black) => {
-  //   for (let j = 0; j < data.length; j++) {
-  //     if (data[j].roomId === roomId) {
-  //       flag = j;
-  //       break;
-  //     }
-  //   }
-  //   if (black) {
-  //     let count = 0;
-  //     let arr1 = [];
-  //     for (let i = 0; count < 1; i++) {
-  //       const number = Math.floor(Math.random() * 13);
-  //       if (data[flag].blackCardList[number] === null) {
-  //         data[flag].blackCardList[number] = userId;
-
-  //         count++;
-  //       }
-  //     }
-  //   } else {
-  //     count = 0;
-  //     for (let i = 0; count < 1; i++) {
-  //       number = Math.floor(Math.random() * 13);
-
-  //       if (data[flag].whiteCardList[number] === null) {
-  //         data[flag].whiteCardList[number] = userId;
-
-  //         count++;
-  //       }
-  //     }
-  //   }
-  // });
-
-  // socket.on("selectUser", (userId, getCard) => {
-  //   for (i = 0; i < gamingUser.length; i++) {
-  //     if (gamingUser[i].userId == userId) {
-  //       getCard(gamingUser[i]);
-  //       break;
-  //     }
-  //   }
-  // });
 });
 
 server.listen(3001, () => {
