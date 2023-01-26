34e1ebdeaeabeb4a5565f986998f7a8fc2538a2b
origin <yesk31@naver.com>
2023-01-26 16:23:44 +0900

    test: mongoose

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const User = require("./users")
+const User = require("./users");
+const Room = require("./rooms");
 
 const app = express();
 dotenv.config();
@@ -20,28 +21,83 @@ DB.on("error", function (err) {
   console.log("DB ERROR: ", err);
 });
 
-const me = new User({
-  userId: 2,
+// const me = new User({
+//   userId: 2,
 
-  sids: "sids-connect-test",
+//   sids: "sids-connect-test",
 
-  username: "haha",
+//   username: "haha",
 
-  isReady: false,
+//   isReady: false,
 
-  isAlive: true,
+//   isAlive: true,
 
-  hand: [],
+//   hand: [],
+// });
 
+const testroom = new Room({
+  roomId: 3,
+
+  turn: 2,
+
+  table: {
+    blackCards: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    whiteCards: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    users: [3, 2, 1, 4],
+  },
+
+  users: [
+    {
+      userId: 0,
+      sids: 0,
+      username: 0,
+      isReady: false,
+      isAlive: true,
+      hand: [], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+    },
+    {
+      userId: 1,
+      sids: 0,
+      username: 0,
+      isReady: false,
+      isAlive: true,
+      hand: [], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+    },
+    {
+      userId: 2,
+      sids: 0,
+      username: 0,
+      isReady: false,
+      isAlive: true,
+      hand: [], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+    },
+    {
+      userId: 3,
+      sids: 0,
+      username: 0,
+      isReady: false,
+      isAlive: true,
+      hand: [], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+    },
+  ],
 });
 
-me.save()
+testroom
+  .save()
   .then(() => {
-    console.log(me);
+    console.log(testroom);
   })
   .catch((err) => {
     console.log("Error : " + err);
   });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((err) => {
+//     console.log("Error : " + err);
+//   });
 
 app.listen(3000, function () {
   console.log("server on! https://localhost:3000");
--- schemas/rooms.js
+++ schemas/rooms.js
@@ -16,6 +16,17 @@ const roomSchema = new mongoose.Schema({
     whiteCards: [Number],
     users: [Number],
   },
+
+  users: [
+    {
+      userId: Number,
+      sids: Number,
+      username: String,
+      isReady: Boolean,
+      isAlive: Boolean,
+      hand: [{ color: String, value: Number, isOpen: Boolean }], // [ {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true}, {color: black, value: 3 , isOpen: true} ]
+    },
+  ],
 });
 
 module.exports = mongoose.model("room", roomSchema);
